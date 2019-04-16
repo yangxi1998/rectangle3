951c3684b4a1d7ebe85b015f045d924480d4abfb
yangxi1998 <2462006777@qq.com>
2019-04-16 16:56:55 +0800

    v0.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,10 +19,21 @@ module.exports = function (grunt) {
         },
         src: ['*.html']
       }
+    },
+    mocha: {
+      test: {
+        src: ['test/index.html'],
+      },
+      options: {
+        run: true,
+        reporter: 'Spec'
+      }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-csslint');
-  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-htmlhint');  
   grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-mocha');
   grunt.registerTask('default', ['htmlhint', 'csslint', 'eslint']);
+  grunt.registerTask('default', ['mocha']);
 };
--- rectangle.js
+++ rectangle.js
@@ -1,11 +1,10 @@
 $(function() {
   var $width=$('#width'),$height=$('#height'),$btnCal=$('#calcute'),$perimeter=$('#perimeter'),$area=$('#area');
   $btnCal.click(function(){
-    var w=Number($width.val()),
-        h=Number($height.val());
-    var p=2*(w+h),
-        a=w*h; 
-    $perimeter.val(p);
-    $area.val(a);
+    var w=$width.val(),
+        h=$height.val();
+    var r = new Rectangle(w,h);//eslint-disable-line no-undef
+    $perimeter.val(r.perimeter());
+    $area.val(r.area());
   });
 });
