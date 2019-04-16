5e377bc7af263a81ee6a9c0e349806970f9c6fad
yangxi1998 <2462006777@qq.com>
2019-04-16 17:10:13 +0800

    v0.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,12 +28,37 @@ module.exports = function (grunt) {
         run: true,
         reporter: 'Spec'
       }
-    }
+    },
+    htmlmin: {
+      options: {
+        collapseWhitespace: true,
+        preserveLineBreaks: false
+      },
+      files: {
+        src: './index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    cssmin: {
+      'dist/rectangle.css': 'rectangle.css'
+    },
+    uglify: {
+      release:{
+        files: {
+          'dist/rectangle.js': 'rectangle.js',
+          'dist/calc.js': 'calc.js'
+        }
+      }
+    }     
   });
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-htmlhint');  
   grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.registerTask('default', ['htmlhint', 'csslint', 'eslint']);
   grunt.registerTask('default', ['mocha']);
+  grunt.registerTask('default', ['htmlmin', 'cssmin', 'uglify']);
 };
